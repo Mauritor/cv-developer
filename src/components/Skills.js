d1ccdb324af26bb3e4fdcaf90c7299070374ae28
Mauro <mrtelefonia@gmail.com>
2020-11-16 22:11:29 -0300

    Agregado de mas items skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -36,16 +36,20 @@ const Skills = () => {
                             <div style={{fontSize: 36, color: '#569234'}}>Mongodb</div>
                             <div style={{fontSize: 24, color: '#002D4B'}}>Sqlite</div>
                             <div style={{fontSize: 34, color: '#F6BE00'}}>Firebase</div>
-                            <div style={{fontSize: 18, color: '#00B3E9'}}>JWT</div>
+                            <div style={{fontSize: 20, color: '#00B3E9'}}>JWT</div>
                             <div style={{fontSize: 42, color: '#533B78'}}>Bootstrap</div>
                             <div style={{fontSize: 24, color: '#254BDD'}}>Css</div>
                             <div style={{fontSize: 40, color: '#DD4B25'}}>HTML5</div>
                             <div style={{fontSize: 24, color: '#F7F7F7'}}>Cordova Apache</div>
-                            <div style={{fontSize: 28, color: '#3FD9F5'}}>React-Bootstrap</div>
+                            <div style={{fontSize: 32, color: '#3FD9F5'}}>React-Bootstrap</div>
                             <div style={{fontSize: 30, color: '#F76A71'}}>Materialize</div>
                             <div style={{fontSize: 30, color: '#F7F7F7'}}>Font-Awesome</div>
                             <div style={{fontSize: 28, color: '#00B0FF'}}>Material-Ui</div>
                             <div style={{fontSize: 24, color: '#F3BB81'}}>Parcel</div>
+                            <div style={{fontSize: 34, color: '#A91E50'}}>Ejs</div>
+                            <div style={{fontSize: 32, color: '#880000'}}>Mongoose</div>
+                            <div style={{fontSize: 28, color: '#8E75FF'}}>Strapi</div>
+                            <div style={{fontSize: 28, color: '#3492FF'}}>Lodash_</div>
                             <div style={{fontSize: 8, color: 'transparent'}}>{seconds}</div>
                         </TagCloud>
                     </Col>
