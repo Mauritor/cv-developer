a2a062c594481a7abcec2e6044d06beb6b4fea8a
Mauro <mrtelefonia@gmail.com>
2021-01-22 15:07:07 -0300

    Agregado de pdf

--- src/components/JumboAvatar.js
+++ src/components/JumboAvatar.js
@@ -1,19 +1,31 @@
 import React, { Fragment } from 'react';
 import { Jumbotron, Container } from 'react-bootstrap';
 import myavatar from '../utils/img/mau_300x300.png';
+import pdf from '../utils/img/pdf_file.png'
 
 const JumboAvatar = () => {
     return (
         <Fragment>
             <Jumbotron fluid className="bg-primary">
+                <Container className="text-right mt-0">
+                <a href="https://drive.google.com/file/d/1K0HXE14GER-hzrombzbHmGp3x4yJUHNO/view?usp=sharing" target="blank">
+                    <img
+                        width={72}
+                        height={72}
+                        className="avatar-rounded"
+                        src={pdf}
+                        alt="yomesmo"
+                    /></a>
+                </Container>
+
                 <Container className="d-flex justify-content-around">
-                <img
-                    width={100}
-                    height={100}
-                    className="avatar-rounded"
-                    src={myavatar}
-                    alt="yomesmo"
-                />
+                    <img
+                        width={100}
+                        height={100}
+                        className="avatar-rounded"
+                        src={myavatar}
+                        alt="yomesmo"
+                    />
                     <h3 className="text-success mt-4">
                         Mauro Ragusi
                     </h3>
